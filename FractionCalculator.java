516a4097b8138fae639532dc7b40e3943c08c4a3
lamnguyenCIS <lamnguyen.cis136@gmail.com>
2018-09-05 06:06:56 -0700

    Add FractionCalculator.java to the local repos

---
+++ FractionCalculator.java
@@ -0,0 +1,14 @@
+import myfraction.Fraction;
+
+public static void main(String arg[]){
+	Fraction fraction = new Fraction(1,2);
+
+	int numerator = 0;
+	int denominator = 0;
+
+	numeartor = fraction.getNumerator();
+	denominator = fraction.getDenominator();
+
+	System.out.println("This is numerator: " + numerator);
+	System.out.println("This is denominator: " + denominator);
+}
