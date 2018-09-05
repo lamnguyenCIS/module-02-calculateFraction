885962a4bdd61e5a8c32b6fc207eff9494ea9103
lamnguyenCIS <lamnguyen.cis136@gmail.com>
2018-09-05 06:07:54 -0700

    add fraction.java to the local repos

--- Fraction.java
+++ Fraction.java
@@ -1,3 +1,5 @@
+package myfraction;
+
 public class Fraction{
 	int numerator;
 	int denominator;
@@ -28,7 +30,65 @@ public class Fraction{
 		this.denominator = 1;
 	}
 
+	public Integer getNumerator(){
+		return numerator;
+	}
 
+	public Integer getDenominator(){
+		return denominator;
+	}
 
+	public Stringn toString(){
+		return numerator + " / " + denominator;
+	}
+
+	public Double toDouble(){
+		Double result = 0.0;
+
+		result = numeartor / denominator;
+
+		return result;
+	}
+
+	public Fraction add(int numerator, int denominator){
+		Fraction fractionAdd = new Fraction(1,2);
+
+		this.numerator += numeartor;
+		this.denominator += denominator;
+
+		return fractionAdd;
+	}
+
+	public Fraction sub(int numerator, int denominator){
+		Fraction fractionSub = new Fraction(1,2);
+
+		this.numerator -= numeartor;
+		this.denominator -= denominator;
+
+		return fractionSub;
+	}	
+	
+	public Fraction mul(int numerator, int denominator){
+		Fraction fractionMul = new Fraction(1,2);
+
+		this.numerator *= numeartor;
+		this.denominator *= denominator;
+
+		return fractionMul;
+	}	
+
+	public Fraction div(int numerator, int denominator){
+		Fraction fractionDiv = new Fraction(1,2);
+
+		if(denominator == 0){
+			throw new IllegalArgumentException("Denominator cannot be ZERO");
+		}
+		else{
+				this.numerator /= numeartor;
+				this.denominator /= denominator;
+		}
+
+		return fractionDiv;
+	}	
 	
 }
