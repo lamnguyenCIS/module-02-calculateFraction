ed0d53493e526cb14b8334ef1036a9a83b4be9d3
lamnguyen <lamnguyen.cis136@gmail.com>
2018-09-16 09:29:43 -0700

    .

--- Fraction.java
+++ Fraction.java
@@ -1,22 +1,24 @@
+import java.io.*;
+
 public class Fraction{
-	Double numerator;
-	Double denominator;
+	int numerator;
+	int denominator;
 
 	// default constructor
 	// Doubleeger - 0 && double - 0.0 && String null
 	public Fraction(){
-		this.numerator = 0.0;
-		this.denominator = 1.0;
+		this.numerator = 0;
+		this.denominator = 1;
 	}
 
    // 2 parameter constructor
-	public Fraction (Double numerator, Double denominator){
-		if(denominator == 0.0){
+	public Fraction (int numerator, int denominator){
+		if(denominator == 0){
 			throw new IllegalArgumentException("Denominator cannot be ZERO");
 		}
-		else if(denominator < 0.0){
-			this.numerator = numerator * (-1.0);
-			this.denominator = denominator * (-1.0);
+		else if(denominator < 0){
+			this.numerator = numerator * (-1);
+			this.denominator = denominator * (-1);
 		}
 		else{
 			this.numerator = numerator;
@@ -25,94 +27,75 @@ public class Fraction{
 	}
 	
     // One parameter constructor
-	public Fraction (Double numerator){
+	public Fraction (int numerator){
 		this.numerator = numerator;
-		this.denominator = 1.0;
+		this.denominator = 1;
 	}
  
-	public Double getNumerator(){
+	public int getNumerator(){
 		return numerator;
    }	
 
-	public Double getDenominator(){
+	public int getDenominator(){
 		return denominator;
 	}
 
-   // PrDouble the string format of the fraction
+   // Print the string format of the fraction
 	public String toString(){
 		return this.numerator + " / " + this.denominator;
 	}
-}
 
-/*
 
-public class Fraction{
-	Double numerator;
-	Double denominator;
-
-	// default constructor
-	// Doubleeger - 0 && double - 0.0 && String null
-	public Fraction(){
-		this.numerator = 0.0;
-		this.denominator = 1.0;
-	}
-
-   // 2 parameter constructor
-	public Fraction (Double numerator, Double denominator){
-		if(denominator == 0.0){
-			throw new IllegalArgumentException("Denominator cannot be ZERO");
-		}
-		else if(denominator < 0.0){
-			numerator = numerator * (-1.0);
-			denominator = denominator * (-1.0);
-		}
-		else{
-			Fraction myFrac = new Fraction(4.0,5.0);
-		}
-
-	}
-	
-   // One parameter constructor
-	public Fraction (Double numerator){
-		Fraction myFrac = new Fraction(numerator);
-		this.denominator = 1.0;
-	}
-
-   // 
-	public Double getNumerator(){
-		return numerator;
-	}
-
-	public Double getDenominator(){
-		return denominator;
-	}
-
-   // PrDouble the string format of the fraction
-	public String toString(){
-		return numerator + " / " + denominator;
-	}
-
-   // PrDouble the result of the fraction in double value
+	   // Print the result of the fraction in int value
 	public Double toDouble(){
 		Double result = 0.0;
 
-		result = numerator / denominator;
+		result = Double.valueOf(numerator) / Double.valueOf(denominator);
 
 		return result;
 	}
 
    // Add 2 fraction 
-	public Fraction add(Double numerator, Double denominator){
-		Fraction fractionAdd = new Fraction(1.0,2.0);
+	public Fraction add(Fraction fract1){
+		int num = this.numerator*fract1.denominator + this.denominator*fract1.numerator;
+		int den = this.denominator*fract1.denominator ;
 
-		this.numerator += numerator;
-		this.denominator += denominator;
+		Fraction fractionAdd = new Fraction(num,den);
 
 		return fractionAdd;
+	}	
+
+   // Subtract 2 fraction
+	public Fraction sub(Fraction fract1){
+		int num = this.numerator*fract1.denominator - this.denominator*fract1.numerator;
+		int den = this.denominator*fract1.denominator ;
+
+		Fraction fractionSub = new Fraction(num,den);
+
+		return fractionSub;
+	}	
+
+
+
+}
+
+/*
+
+	public Boolean equals(Object obj){
+		
+    	if(string.contains("/")){
+    		String[] parts = string.split("/");
+            for(int i=0;i<parts.length;i++){
+              System.out.println(parts[i]);
+            }			
+    	}
+    	else{
+    		System.out.println("It doesn't have a slash");
+    	}
 	}
 
    // Subtract 2 fraction
-	public Fraction sub(Double numerator, Double denominator){
+	public Fraction sub(intnumerator, intdenominator){
 		Fraction fractionSub = new Fraction(1.0,2.0);
 
 		this.numerator -= numerator;
@@ -122,7 +105,7 @@ public class Fraction{
 	}	
 	
    // Multiply 2 fraction
-	public Fraction mul(Double numerator, Double denominator){
+	public Fraction mul(intnumerator, Double denominator){
 		Fraction fractionMul = new Fraction(1.0,2.0);
 
 		this.numerator *= numerator;
--- FractionCalculator.java
+++ FractionCalculator.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
+
 public class FractionCalculator{
 	public static void main (String[] args){
-		Fraction fraction = new Fraction(-1.0,4.0);
-      Fraction fraction1 = new Fraction(2.0);
-       
-       
-		Double numerator = 0.0;
-		Double denominator = 0.0;
-      Double res = 0.0;
+
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Enter numerator: ");
+		int numerator = reader.nextInt();
+
+		System.out.print("Enter denominator: ");
+		int denominator = reader.nextInt();
+
+		Fraction fraction = new Fraction(numerator,denominator);
+
+		
+        Double res = 0.0;
 
 		numerator = fraction.getNumerator();
 		denominator = fraction.getDenominator();
@@ -14,13 +21,8 @@ public class FractionCalculator{
 		System.out.println("This is numerator: " + numerator);
 		System.out.println("This is denominator: " + denominator);
 
-		String output = fraction.toString();
-		System.out.println(output);
-
-		String output1 = fraction1.toString();
-		System.out.println(output1);
-
 		res = fraction.toDouble();
 		System.out.println(res);
+
 	}
 }
